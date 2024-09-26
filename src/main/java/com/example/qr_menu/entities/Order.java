cdfbff31ddee6dd7a45a7c72af0f62281a21d731
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2024-09-26 12:36:38 +0300

    adding orders in to db

---
+++ src/main/java/com/example/qr_menu/entities/Order.java
@@ -0,0 +1,46 @@
+package com.example.qr_menu.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "order_time", nullable = false)
+    private Date orderTime = new Date();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "order_status", nullable = false)
+    private OrderStatus orderStatus;
+
+    @Column(name = "total_price", nullable = false)
+    private Long totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @ManyToOne
+    @JoinColumn(name = "restorant_id", nullable = false)
+    private Restorant restorant;
+
+    public enum OrderStatus {
+        FINISHED,
+        ACCEPTED
+    }
+}
