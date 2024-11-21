ca4c8f36e75c198dba423045f2bf4206c81eb9b8
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2024-11-21 17:06:27 +0200

    can log in  with acc name or  email adress

--- src/main/java/com/example/qr_menu/repositories/AccountRepository.java
+++ src/main/java/com/example/qr_menu/repositories/AccountRepository.java
@@ -2,6 +2,7 @@ package com.example.qr_menu.repositories;
 
 import com.example.qr_menu.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
+    @Query("SELECT a FROM Account a WHERE a.accountName = ?1 OR a.mailAddress = ?1")
     Optional<Account> findByAccountNameOrMailAddress(String accountName, String mailAddress);
 
     // Custom query methods can be added here if needed
