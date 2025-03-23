a8ebc99ea508a0a013702a177036583c1af90780
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2025-03-23 20:50:17 +0200

    Update Favorites.jsx

--- front_end/src/pages/Favorites.jsx
+++ front_end/src/pages/Favorites.jsx
@@ -203,7 +203,7 @@ const Favorites = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
               {paginatedFavorites.map((favorite) => (
                 <ProductCard
-                  key={favorite.uniqueKey}
+                  key={favorite.id || favorite.productId}
                   product={favorite}
                   onSelectProduct={handleSelectProduct}
                   onEditProduct={() => {}}
